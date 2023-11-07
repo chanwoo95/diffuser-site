7e6f8e18c4c2e478f7cd4e1fc885ff8c9fdb4f95
chanwoo95 <cksdndbd95@gmail.com>
2023-11-07 23:18:51 +0900

    firebase api

--- src/api/firebase.js
+++ src/api/firebase.js
@@ -1,4 +1,5 @@
 import { initializeApp } from "firebase/app";
+import { get, getDatabase, ref, set } from "firebase/database";
 import {
   GoogleAuthProvider,
   getAuth,
@@ -6,6 +7,7 @@ import {
   signInWithPopup,
   signOut,
 } from "firebase/auth";
+import { v4 as uuidv4 } from "uuid";
 
 const firebaseConfig = {
   apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
@@ -15,6 +17,7 @@ const firebaseConfig = {
 
 const firebase = initializeApp(firebaseConfig);
 const auth = getAuth(firebase);
+const database = getDatabase(firebase);
 
 const provider = new GoogleAuthProvider();
 
@@ -31,5 +34,32 @@ export function onAuthChanged(callback) {
     // 로그인한 유저 정보가 있다면 유지
     // 유저 정보가 없다면 null
     callback(user);
+  });
+}
+
+export async function getProducts() {
+  return get(ref(database, "products")) //
+    .then((snapshot) => {
+      if (snapshot.exists()) {
+        const val = snapshot.val();
+        return Object.values(val);
+      }
+    });
+}
+
+export async function addCart(product, quantity) {
+  const id = uuidv4();
+  return set(ref(database, `carts/${id}`), {
+    ...product,
+    id,
+    quantity,
+  });
+}
+
+export async function loadCart() {
+  return get(ref(database, "carts")).then((snapshot) => {
+    if (snapshot.exists()) {
+      return Object.values(snapshot.val());
+    }
   });
 }
