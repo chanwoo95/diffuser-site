5184e4b9bc3b7a746ebb817555d8624320ce9c84
chanwoo95 <cksdndbd95@gmail.com>
2023-11-08 23:46:03 +0900

    main page ui

--- src/components/Main/Main.jsx
+++ src/components/Main/Main.jsx
@@ -1,5 +1,31 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+import { getProducts } from "../../api/firebase";
+import Products from "../Products/Products";
 
 export default function Main() {
-  return <div>main</div>;
+  const [products, setProducts] = useState();
+
+  useEffect(() => {
+    getProducts().then((items) => setProducts(items));
+  }, []);
+
+  return (
+    <div className="absolute top-0">
+      <div className=" w-full overflow-hidden">
+        <img
+          src="/images/main_homet.jpg"
+          alt=""
+          className="w-full max-sm:h-[400px] "
+        />
+      </div>
+      <div className="p-4">
+        <h1 className="text-center">Best Seller</h1>
+        <ul className="grid grid-cols-4 gap-4">
+          {products &&
+            products.map((product) => <Products product={product} />)}
+        </ul>
+        <p>dd</p>
+      </div>
+    </div>
+  );
 }
