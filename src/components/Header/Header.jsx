629aa4c7b4f2651aa655babdbd2989b032dae4d1
chanwoo95 <cksdndbd95@gmail.com>
2023-11-16 23:46:04 +0900

    fix header

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -6,45 +6,55 @@ import { BiImageAdd } from "react-icons/bi";
 import { BsBag } from "react-icons/bs";
 import Button from "./Button/Button";
 import { login, logout } from "../../api/firebase";
+import Menu from "./Menu/Menu";
+import { useAuthContext } from "../../context/AuthProvider";
 
 export default function Header() {
-  const [user, setUser] = useState();
+  const { user, login, logout } = useAuthContext();
 
-  const handleLogin = () => {
-    login().then((result) => {
-      setUser(result);
-      console.log(result);
-    });
-  };
-
-  const handleLogout = () => {
-    logout().then(() => setUser(null));
-  };
-
-  // useEffect(() => {
-
-  // }, [])
+  const [active, setActive] = useState(false);
 
   return (
-    <header className="flex justify-between p-5 border-slate-500 border-y-2">
-      <Link to="/">
-        <p>Logo</p>
-      </Link>
-      <div className="flex items-center text-xl gap-5">
+    <header className="fixed w-full p-5 border-slate-500 border-y-2 z-10 bg-slate-50 ">
+      <div className="flex justify-between">
         <Link to="/">
-          <AiOutlineUser />
+          <p>Logo</p>
         </Link>
-        <Link to="/basket">
-          <BsBag />
-        </Link>
-        <Link to="/new">
-          <BiImageAdd />
-        </Link>
-        <Button
-          text={user ? "Logout" : "Login"}
-          login={user ? handleLogout : handleLogin}
-        />
+        <ul className="flex gap-11 max-md:hidden">
+          <li>
+            <Link to="/">Home</Link>
+          </li>
+          <li
+            onMouseEnter={() => setActive(true)}
+            onMouseLeave={() => setActive(false)}
+          >
+            <Link>Shop</Link>
+          </li>
+          <li>
+            <Link>Board</Link>
+          </li>
+          <li>
+            <Link>Review</Link>
+          </li>
+        </ul>
+        <div className="flex items-center text-xl gap-5">
+          <Link to="/">
+            <AiOutlineUser />
+          </Link>
+          <Link to="/basket" className="flex items-center">
+            <BsBag />
+            <p className="pl-2">0</p>
+          </Link>
+          <Link to="/new">
+            <BiImageAdd />
+          </Link>
+          <Button
+            text={user ? "Logout" : "Login"}
+            login={user ? logout : login}
+          />
+        </div>
       </div>
+      {active ? <Menu /> : ""}
     </header>
   );
 }
